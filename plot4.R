64490122e620f69e23d9177430e5b07a9d11c0e4
Eugene2015 <loyolla@gmail.com>
2015-06-19 14:58:35 -0700

    Plot 4, image and implementation code

---
+++ plot4.R
@@ -0,0 +1,75 @@
+# Function that builds 4th plot. Assumes that package 'ggplot2' is loaded into workspace
+# Parameters :
+#             dir.working      [optional] - folder that will contain generated image
+#             output.file.name [optional] - name of the output file
+#             image.width      [optional] - width of the image to be generated
+#             image.height     [optional] - height of the image to be generated
+plot4<-function(dir.working = "./", output.file.name="plot4.png", image.width=800, image.height=600){
+    
+    subfolder = "./dataset";
+    resolution = 150;
+    
+    archive.file.name = sprintf("%s/archive.zip", subfolder);
+    dataset.file.name.1 = sprintf("%s/summarySCC_PM25.rds", subfolder);
+    dataset.file.name.2 = sprintf("%s/Source_Classification_Code.rds", subfolder);
+    # save current and set working directory     
+    dir.current<-getwd();
+    setwd(dir.working);
+    
+    # the very first step - make sure that dataset file exists 
+    if(!file.exists(dataset.file.name.1) | !file.exists(dataset.file.name.2) ){
+        
+        # file does not exists. download one        
+        # -- dataset will be put into subdirectory. create one if necessary 
+        if(!file.exists(subfolder)){
+            dir.create(subfolder);
+        }
+        
+        # --  download archive file that contains dataset
+        download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", dest=archive.file.name, method="curl", quiet=T);
+        if(!file.exists(archive.file.name)){
+            stop("unable to download dataset archive file.");
+        }
+        print(getwd());
+        
+        # -- extract file from archive and verify that extraction succeeded
+        unzip(archive.file.name, junkpaths = T, exdir=subfolder);
+        if(!file.exists(dataset.file.name.1) | !file.exists(dataset.file.name.2)){
+            stop("unable to unzip dataset archive file");
+        }
+        
+        # -- no need for archive file any more
+        unlink(archive.file.name);
+    }
+
+    # second step - prepare dataset
+    # -- load dataset
+    dsNEI <- readRDS(dataset.file.name.1);
+    dsSCC <- readRDS(dataset.file.name.2);
+    
+    # -- filter datasets
+    dsSCC<-dsSCC[grepl("Coal", dsSCC$EI.Sector, ignore.case=T), ];
+    filter<-dsNEI$SCC %in% dsSCC$SCC
+    dsNEI <- dsNEI[filter,];
+  
+    # -- convert year column to factor so it will be labeled properly.
+    dsNEI$year<-factor(dsNEI$year);
+
+    # generate image
+    # -- draw it
+    pl<- ggplot(dsNEI, aes(x=year, y=Emissions)) + 
+         stat_summary(fun.y="mean", geom = "bar", fill="steelblue", alpha=1/2) +  
+         ggtitle("Changes in emission from coal combustion-related sources") + 
+         ylab(expression("Average amount of " * PM[2.5] * " emitted, tons"));
+    
+    # -- save ti
+    ggsave(filename = output.file.name, plot = pl, width = image.width/resolution, height=image.height/resolution, dpi=resolution);
+
+    # restore original working directory
+    setwd(dir.current);
+    
+    # build return value
+    paste(dir.working, output.file.name, sep="");
+    normalizePath(paste(dir.working, output.file.name, sep=""));
+}
+    
